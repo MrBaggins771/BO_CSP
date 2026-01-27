dd7c79ebfe2c0f829ba9b3107760f3fbaece5991
MrBaggins771 <brody.olson@ucas-edu.net>
2026-01-27 11:51:27 -0700

    changes

---
+++ notes/strings.py
@@ -0,0 +1,22 @@
+"""color1 = input("What is your favorite color? ").strip().upper()
+color2 = input("What is your favorite color? ").strip().lower()
+color3 = input("What is your favorite color? ").strip().capitalize()
+color4 = input("What is your favorite color? ").strip().title()
+print(color1, "is a really cool color!")"""
+
+alphabet = "abcdefghijklmnopqrstuvwxyz"
+sentence = "the quick brown fox jumps over the lazy dog."
+"""print(alphabet[4:8])
+word = input("What word do you want to change? ")
+new = input("What will the new word be? ")
+start = sentence.find(word)
+print("("+sentence[start:start+len(word)]+")")
+print(sentence)
+sentence = sentence.replace(word,new)
+print(sentence)"""
+
+number = int(input("tell me a number: "))
+print(number/5)
+
+room = "Ms.LaRoss's room"
+quote = 'Treyson said, "Yolo"'
--- practices/financial_calc.py
+++ practices/financial_calc.py
@@ -11,6 +11,7 @@ u_percent = round((utilities/m_income)*100, 2)
 g_percent = round((groceries/m_income)*100, 2)
 t_percent = round((transportation/m_income)*100, 2)
 invest = round(float(m_income*.15), 2)
+spending = round(m_income-(housing+utilities+groceries+transportation+invest), 2)
 
 print("Your housing is $", housing, "which is", h_percent, "%", "of your income")
 print("Your utilities are $", utilities, "which is", u_percent, "%", "of your income")
@@ -18,3 +19,4 @@ print("Your groceries are $", groceries, "which is", g_percent, "%", "of your in
 print("Your transportation is $", transportation, "which is", t_percent, "%", "of your income")
 
 print("You should invest $", invest, "which is 15% of your income.")
+print("You have $", spending, "left over.")
