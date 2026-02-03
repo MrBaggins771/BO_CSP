9edd6c17e01a28259744c20a3eb69588b3965adc
MrBaggins771 <brody.olson@ucas-edu.net>
2026-02-03 11:26:30 -0700

    fizzy

---
+++ project/fizzbuzz.py
@@ -0,0 +1,13 @@
+# BO fizz buzz
+
+num = 0
+while num < 50:
+    num += 1
+    if num % 3 == 0 and num % 5 == 0: 
+        print("FizzBuzz")
+    elif num % 5 == 0:
+        print("Buzz")
+    elif num % 3 == 0: 
+        print("Fizz")
+    else:
+        print(num)
