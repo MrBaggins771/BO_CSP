e06222ba850dd671b4b355343dac821ff13e3538
MrBaggins771 <brody.olson@ucas-edu.net>
2026-02-10 11:49:16 -0700

    Co-authored-by: GAC17 <GAC17@users.noreply.github.com>

--- project/team_game.py
+++ project/team_game.py
@@ -64,15 +64,82 @@ def question():
 def area1():
     print(f"You have made it to Silentville.")
     print(f"There are 3 buildings, a Shop, a Dungeon, and a Barbershop.")
+    answ = question()
+    return answ
+
+def area2():
+    print("You're heading to the castle, you can turn back or go forwards")
+    choice = question()
+    if choice == "backwards":
+       print ("You turn back")
+    elif choice == "forwards":
+       print("You begin to face the castle")
+    elif choice == "left" or choice == "right":
+        print("You can't do that here, try forwards or backwards.")
+    else:
+        print("You can't do that here")
+
+def shop():
+    s_value = 0
+    print(f"Welcome to Silentvilles' local shop")
+    stuff = input("Are you looking to buy anything?").strip().lower()
+    if stuff == "yes":
+        s_value += 1
+        print("We've got a lot, but based on your apperance here pal, you're not buying anything at this establishment.")
+    elif stuff == "no":
+        s_value += 1
+        print("No, brokie")
+    else:
+        s_value +=1
+        print("What are you doing here then?")
+        return s_value
+    
+def b_shop():
+    b_value = 0
+    print(f"Welcome to Silentvilles' Barbershop!")
+    print("What are you looking for today?")
+    cut = input("You tryna look fly?").strip().lower()
+    if cut == "yes":
+        b_value += 1
+        print("Heres your cut yo")
+    elif cut == "no":
+        b_value += 1
+        print("Leave my shop, pooron")
+    else:
+        b_value += 1
+        print("What you talking about yo")
+    return b_value
 
 def game():
     name = input("What is your name? ")
     print(f"Hello {name} welcome to {kingdom}.")
     controls()
-    location = area1()
-    area1_answ = question()
-    if area1_answ == "backwards":
-        print("You can't do that.")
-    elif area1_answ
+    in_area1 = 1
+    in_area2 = 0
+    while in_area1 == 1:
+        area1_answ = area1()
+        if area1_answ == "backwards":
+            print("You can't do that here.")
+        elif area1_answ == "left":
+            print("You go to the shop.")
+            s_value = shop()
+            if s_value > 0:
+                area1()
+        elif area1_answ == "right":
+            print("You go to the Barbershop.")
+            b_value = b_shop()
+            if b_value > 0:
+                area1
+        elif area1_answ == "forwards":
+            print("You go fowards.")
+        else:
+            print("placement")
+            break
+    in_area1 -= 1
+    if in_area1 != 1:
+        in_area2 += 1
+    if in_area2 == 1:
+        area2()
+    
 
 game()
