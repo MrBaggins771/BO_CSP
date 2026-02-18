d34141d31f6bb038c9339cfddeb14c4f529a83e7
MrBaggins771 <brody.olson@ucas-edu.net>
2026-02-18 11:49:10 -0700

    aahhhhhhh

---
+++ C/notes/variables.c
@@ -0,0 +1,10 @@
+// This is a comment in C
+#include <stdio.h> // alows inputs and outputs
+
+int main(){
+    int number = 12;
+    float pi = 3.14;
+    printf("%d\n", number); // \n makes new line
+    printf("%f\n", pi);
+    return 0; // always last line of main
+}
