acfd9f49bb72a2b8e3c750104b8a10301f89b84f
MrBaggins771 <brody.olson@ucas-edu.net>
2026-02-19 11:47:38 -0700

    c is silly

--- C/notes/variables.c
+++ C/notes/variables.c
@@ -4,7 +4,18 @@
 int main(){
     int number = 12;
     float pi = 3.14;
+    char name[] = "Xavier";
+    char person[50];
+    int age;
+
+    printf("Age?\n");
+    scanf("%d", &age);
+    printf("Name?\n");
+    scanf("%s", &person);
+
     printf("%d\n", number); // \n makes new line
     printf("%f\n", pi);
+    printf("%s is %d years old.\n", name, age);
+    printf("Name is %s and is %d years old.", person, age);
     return 0; // always last line of main
 }
---
+++ C/practices/variables_practice.c
@@ -0,0 +1,17 @@
+// BO 3rd, variables practice.
+#include <stdio.h>
+
+int main(){
+    char name[] = "Brody";
+    int number = 4;
+    float gpa = 3.9;
+    char breakfast[] = "Eggs";
+    char fav_color[] = "purple";
+    char school_name[] = "UCAS";
+    int year = 2026;
+    char eye_color[] = "Green";
+    int age = 15;
+    char fav_subject[] = "Drawing";
+    printf("Hello my name is %s and I am %d years old. My favorite color, number, and subject are %s, %d, and %s. My eyes are %s and I had %s for breakfast. I attend %s as a sophmore in the year %d and have a %f GPA.", name, age, fav_color, number, fav_subject, eye_color, breakfast, school_name, year, gpa);
+    return 0;
+}
