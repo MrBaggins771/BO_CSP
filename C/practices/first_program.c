6b8b00ce75bf2e3d4d2b7af98dc8de9358fc9a56
MrBaggins771 <brody.olson@ucas-edu.net>
2026-02-25 11:13:58 -0700

    First Program done!

---
+++ C/practices/first_program.c
@@ -0,0 +1,8 @@
+// BO 3 first program
+#include <stdio.h>
+int main(){
+    char name [50];
+    printf("What is your name? ");
+    scanf("%s", &name);
+    printf("\nHello %s, thats a cool name!", name);
+}
