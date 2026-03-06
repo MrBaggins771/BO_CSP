b0908db63a95c74b686efd4b97913f414b84c743
MrBaggins771 <brody.olson@ucas-edu.net>
2026-03-06 11:18:39 -0700

    calc is short for calculator update

--- C/practices/update_calc.c
+++ C/practices/update_calc.c
@@ -2,20 +2,23 @@
 #include <stdio.h>
 #include <math.h>
 
+//Function for obtaining information
 float question(char*cost){
     float answer;
-    printf("What is your monthy %s?\n", cost);
+    printf("\nWhat is your monthy %s? $", cost);
     scanf("%f", &answer);
     return answer;
 }
 
-int percent_calc(int cost, int income){
-    int percent = (cost / income) * 100;
+//Function for percent calculations
+float percent_calc(float cost, float income){
+    float percent = (cost / income) * 100;
     return percent;
 }
 
-void print(char*cost_name, float cost, int cost_percent){
-    printf("Your monthly %s is %.2f which is %d percent of your income.", cost_name, cost, cost_percent)
+//Function giving information back
+void printc(char*cost_name, float cost, float cost_percent){
+    printf("\nYour monthly %s bill is $%.2f which is %.2f percent of your income.", cost_name, cost, cost_percent);
 }
 
 int main(){
@@ -25,9 +28,20 @@ int main(){
     float groceries = question("groceries");
     float transit = question("transit");
     
-    int housing_p = percent_calc(housing, income);
-    int utilities_p = percent_calc(utilities, income);
-    int groceries_p = percent_calc(groceries, income);
-    int transit_p = percent_calc(transit, income);
+    float housing_p = percent_calc(housing, income);
+    float utilities_p = percent_calc(utilities, income);
+    float groceries_p = percent_calc(groceries, income);
+    float transit_p = percent_calc(transit, income);
+
+    printc("housing", housing, housing_p);
+    printc("utilities", utilities, utilities_p);
+    printc("grocery", groceries, groceries_p);
+    printc("transit", transit, transit_p);
+    
+    float savings = income * 0.15;
+    float spending = income - (housing + utilities + groceries + transit + savings);
+    printf("\nYou should save $%.2f, which is 15 percent of your income.", savings);
+    printf("\nYou have $%.2f left to spend.", spending);
+    
     return 0;
 }
