142a08c4e97092caeeda0ac348c61d03c3d9dee1
MrBaggins771 <brody.olson@ucas-edu.net>
2026-03-18 11:47:55 -0600

    Co-authored-by: manny142 <manny142@users.noreply.github.com>

--- C/practices/final_project.c
+++ C/practices/final_project.c
@@ -4,10 +4,21 @@
 #include <time.h>
 #include <string.h>
 
+
+char spot1 = '_';
+char spot2 = '_';
+char spot3 = '_';
+char spot4 = '_';
+char spot5 = '_';
+char spot6 = '_';
+char spot7 = '_';
+char spot8 = '_';
+char spot9 = '_';
+
 void grid (){
     printf("_%c_|_%c_|_%c_\n", spot1, spot2, spot3);
     printf("_%c_|_%c_|_%c_\n", spot4, spot5, spot6);
-    printf("_%c_|_%c_|_%c_\n", spot7, spot8, spot9);
+    printf(" %c | %c | %c \n", spot7, spot8, spot9);
 }
 
 int bot(){
@@ -23,71 +34,144 @@ int player(){
     return answer;
 }
 
-void big_spot_check(int answer, int bot_or_player){
-        if (answer = 1){
-            int check = spot_check(spot1);
-            if (check = 1 && bot_or_player = 0){
-                char spot1 = "X"
-            }else if (check = 1 && bot_or_player = 1){
-                char spot1 = "O"
-            }else{
-                printf("not valid")
-            }
-        }else if (player_answer = 2){
-            int check = spot_check(spot2);
-        }else if (player_answer = 3){
-            int check = spot_check(spot3);
-        }else if (player_answer = 4){
-            int check = spot_check(spot4);
-        }else if (player_answer = 5){
-            int check = spot_check(spot5);
-        }else if (player_answer = 6){
-            int check = spot_check(spot6);
-        }else if (player_answer = 7){
-            int check = spot_check(spot7);
-        }else if (player_answer = 8){
-            int check = spot_check(spot8);
-        }else if (player_answer = 9){
-            int check = spot_check(spot9);
-        }else{
-            printf("Please put a valid input.")
-        }
-}
-
 int spot_check(char spot_value){
-    if (spot_value != "_"){
+    if (spot_value != '_'){
         return 0;
     }else{
         return 1;
     }
 }
 
-int main(){
-    char spot1 = "_";
-    char spot2 = "_";
-    char spot3 = "_";
-    char spot4 = "_";
-    char spot5 = "_";
-    char spot6 = "_";
-    char spot7 = "_";
-    char spot8 = "_";
-    char spot9 = "_";
+int big_spot_check(int answer, int bot_or_player){
+    if (answer == 1){
+        int check = spot_check(spot1);
+        if (check == 0){
+            return 0;
+        }else if (check == 1 && bot_or_player == 0){
+            spot1 = 'X';
+            return 1;
+        }else if (check == 1 && bot_or_player == 1){
+            spot1 = 'O';
+            return 1;
+        }
+    }else if (answer == 2){
+        int check = spot_check(spot2);
+        if (check == 0){
+            return 0;
+        }else if (check == 1 && bot_or_player == 0){
+            spot2 = 'X';
+            return 1;
+        }else if (check == 1 && bot_or_player == 1){
+            spot2 = 'O';
+            return 1;
+        }
+    }else if (answer == 3){
+        int check = spot_check(spot3);
+        if (check == 0){
+            return 0;
+        }else if (check == 1 && bot_or_player == 0){
+            spot3 = 'X';
+            return 1;
+        }else if (check == 1 && bot_or_player == 1){
+            spot3 = 'O';
+            return 1;
+        }
+    }else if (answer == 4){
+        int check = spot_check(spot4);
+        if (check == 0){
+            return 0;
+        }else if (check == 1 && bot_or_player == 0){
+            spot4 = 'X';
+            return 1;
+        }else if (check == 1 && bot_or_player == 1){
+            spot4 = 'O';
+            return 1;
+        }
+    }else if (answer == 5){
+        int check = spot_check(spot5);
+        if (check == 0){
+            return 0;
+        }else if (check == 1 && bot_or_player == 0){
+            spot5 = 'X';
+            return 1;
+        }else if (check == 1 && bot_or_player == 1){
+            spot5 = 'O';
+            return 1;
+        }
+    }else if (answer == 6){
+        int check = spot_check(spot6);
+        if (check == 0){
+            return 0;
+        }else if (check == 1 && bot_or_player == 0){
+            spot6 = 'X';
+            return 1;
+        }else if (check == 1 && bot_or_player == 1){
+            spot6 = 'O';
+            return 1;
+        }
+    }else if (answer == 7){
+        int check = spot_check(spot7);
+        if (check == 0){
+            return 0;
+        }else if (check == 1 && bot_or_player == 0){
+            spot7 = 'X';
+            return 1;
+        }else if (check == 1 && bot_or_player == 1){
+            spot7 = 'O';
+            return 1;
+        }
+    }else if (answer == 8){
+        int check = spot_check(spot8);
+        if (check == 0){
+            return 0;
+        }else if (check == 1 && bot_or_player == 0){
+            spot8 = 'X';
+            return 1;
+        }else if (check == 1 && bot_or_player == 1){
+            spot8 = 'O';
+            return 1;
+        }
+    }else if (answer == 9){
+        int check = spot_check(spot3);
+        if (check == 0){
+            return 0;
+        }else if (check == 1 && bot_or_player == 0){
+            spot9 = 'X';
+            return 1;
+        }else if (check == 1 && bot_or_player == 1){
+            spot9 = 'O';
+            return 1;
+        }
+    }
+}
 
+void check_win(){
+    if ())
+}
+
+int main(){
     int round = 0;
 
     while(round <= 10){
         int player_answer = player();
+        int big_check = big_spot_check(player_answer, 0);
+        if (big_check == 1){
+            grid();
+            round++;
+        }else{
+            printf("")
+        }
     }
 
     //input and outputs area of rodot
     srand(time(NULL));
     int x = rand() % 9;
     printf("%d\n", x);
-    
-    //player input
+
     int player;
     printf("what spot do you want to enter (1-9):");
     scanf("%d", player);
+
 
     return 0;
 }
