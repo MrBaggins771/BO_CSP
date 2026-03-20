aa09751b70d4f23927fd117ebc985ff3c8c5cc42
MrBaggins771 <brody.olson@ucas-edu.net>
2026-03-20 11:20:14 -0600

    Co-authored-by: manny142 <manny142@users.noreply.github.com>

--- C/practices/final_project.c
+++ C/practices/final_project.c
@@ -5,6 +5,7 @@
 #include <string.h>
 #include <stdbool.h>
 
+// Variables for later
 char spot1 = '_';
 char spot2 = '_';
 char spot3 = '_';
@@ -17,18 +18,21 @@ char spot9 = '_';
 int p_win_check = 0;
 int b_win_check = 0;
 
+// Prints the board
 void grid (){
     printf("\n_%c_|_%c_|_%c_\n", spot1, spot2, spot3);
     printf("_%c_|_%c_|_%c_\n", spot4, spot5, spot6);
     printf(" %c | %c | %c \n", spot7, spot8, spot9);
 }
 
+// Creates the bot's input
 int bot(){
     srand(time(NULL));
     int x = rand() % 9;
     return x;
 }
 
+// Takes the player's input
 int player(){
     int answer;
     printf("\nWhat spot do you want to enter (1-9): ");
@@ -36,6 +40,7 @@ int player(){
     return answer;
 }
 
+// spot_check checks if the spot is taken or not and returns a 1 if the spot is open
 int spot_check(char spot_value){
     if (spot_value != '_'){
         return 0;
@@ -44,6 +49,7 @@ int spot_check(char spot_value){
     }
 }
 
+// big_spot_check makes sure that the spot inputed by the player or bot and makes it an X or O
 int big_spot_check(int answer, int bot_or_player){
     if (answer == 1){
         int check = spot_check(spot1);
@@ -134,7 +140,7 @@ int big_spot_check(int answer, int bot_or_player){
             return 1;
         }
     }else if (answer == 9){
-        int check = spot_check(spot3);
+        int check = spot_check(spot9);
         if (check == 0){
             return 0;
         }else if (check == 1 && bot_or_player == 0){
@@ -147,6 +153,7 @@ int big_spot_check(int answer, int bot_or_player){
     }
 }
 
+// Checks if the player won
 int check_win(){
     if (spot1 == 'X' && spot2 == 'X' && spot3 == 'X'){
         printf("Player wins!");
@@ -177,6 +184,7 @@ int check_win(){
     }
 }
 
+// Checks if the bot won
 int bot_check_win(){
      if (spot1 == 'O' && spot2 == 'O' && spot3 == 'O'){
         printf("Bot wins! It's random gentor you bum.");
@@ -209,36 +217,49 @@ int bot_check_win(){
 
 int main(){
     int round = 0;
-
+// The main game loop
     while(round <= 10){
+        // Game loop for player's turn
         while(true){
+            if (p_win_check == 1 || b_win_check == 1){
+                round += 10;
+                break;
+            }
             int player_answer = player();
             int big_check = big_spot_check(player_answer, 0);
             if (big_check == 1){
                 grid();
                 int p_win_check = check_win();
+                if (p_win_check == 1 || b_win_check == 1){
+                    round += 10;
+                    break;
+                }
                 round++;
                 break;
             }else{
                 printf("Not valid\n");
             }
         }
-    if (p_win_check == 1){
-        break;
-    }
+        // Game loop for bot's turn
         while (true){
+            if (p_win_check == 1 || b_win_check == 1){
+                round += 10;
+                break;
+            }
             int bot_answer = bot();
             int big_check_bot = big_spot_check(bot_answer, 1);
             if (big_check_bot == 1){
                 grid();
                 int b_win_check = bot_check_win();
+                if (p_win_check == 1 || b_win_check == 1){
+                    round += 10;
+                    break;
+                }
                 round++;
                 break;
             }
         }
-    if (b_win_check == 1){
-        break;
-    }
+        
     }
 
     return 0;
